cbe04afef886309c06a0b95c763c1913ef862ba8
Jordan Saxe <jordansaxe15@gmail.com>
2014-03-20 20:42:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacklaunch::Application.config.secret_key_base = '960017f7b567d003ccbc0284997e715b5fe5c625ecf749a28fa704e87de2996db5ef70b62447ef76b2f2c7d4f58577a0f9fa831b8fca103326c29dc84a4b36bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hacklaunch::Application.config.session_store :cookie_store, key: '_hacklaunch_session'
