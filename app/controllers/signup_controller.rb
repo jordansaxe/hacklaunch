34f8014bc576f98f11762eb22ce6579e5a914e52
Jordan Saxe <jordansaxe15@gmail.com>
2014-03-24 20:24:14 -0400

    Added a sign-up model

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,10 +1,14 @@
 class SignupController < ApplicationController
   def view
+    @hide_form = false
+    if params[:hide_form] == 'true'
+      @hide_form =true
+    end
   end
 
   def show_confirm
     @email = params[:email]
     flash[:notice] = "Thanks for signing up!"
-    redirect_to('/')
+    redirect_to('/?hide_form=true')
   end
 end
